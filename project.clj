418c32aa62f6e73cbfd331b6c4c26f2c65bcf152
Guillaume Buisson <guillaume@vigiglobe.com>
2014-11-28 17:52:00 +0100

    bumped version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject y42/clj-druid "0.1.1-SNAPSHOT"
+(defproject y42/clj-druid "0.1.2-SNAPSHOT"
   :description "Clojure library for Druid.io"
   :url "http://github.com/y42/clj-druid"
   :license {:name "MIT"
