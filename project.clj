0d568b462d55be9f46684c820f93733683a17024
Guillaume Buisson <guillaume@vigiglobe.com>
2015-02-17 10:56:32 +0100

    fixed unit test bumped http-kit version

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [swiss-arrows "1.0.0"]
-                 [http-kit "2.1.16"]
+                 [http-kit "2.1.18"]
                  [prismatic/schema "0.3.3"]
                  [org.clojure/tools.logging "0.3.0"]
                  [org.clojure/data.json "0.2.5"]
