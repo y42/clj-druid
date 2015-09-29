8cca4c5ddf4a635a45b8bc7b1c8f2d2eee087d96
Guillaume Buisson <guillaume@vigiglobe.com>
2015-09-29 15:12:32 +0200

    added experimental spatial filter support

--- src/clj_druid/schemas/filter.clj
+++ src/clj_druid/schemas/filter.clj
@@ -23,12 +23,27 @@ Selector filters can be used as the base filters for more complex Boolean expres
    :dimension s/Str
    :function s/Str})
 
+                                        ;Experimental
+
+(s/defschema spatialFilter
+  {:type (s/enum :spatial)
+   :dimension s/Str
+   :bound (s/either
+           {:type (s/enum :rectangular)
+            :minCoords [Float]
+            :maxCoords [Float]}
+
+           {:type (s/enum :radius)
+            :coords [Float]
+            :radius Float})})
+
 (s/defschema Filter
   "A filter is a JSON object indicating which rows of data should be included in the computation for a query.
  It’s essentially the equivalent of the WHERE clause in SQL. Druid supports the following types of filters."
   (s/either selectorFilter
             regexFilter
             javascriptFilter
+            spatialFilter
             {:type (s/enum :not)
              :field (s/recursive #'Filter)}
             {:type (s/enum :or :and)
