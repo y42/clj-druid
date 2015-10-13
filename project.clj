09d13aff0d3ed49f9e199ed1138cda4e785e58c2
Guillaume Buisson <guillaume@vigiglobe.com>
2015-10-13 11:22:27 +0200

    upgraded deps, stick to schema 0.4.4 because of https://github.com/Prismatic/schema/issues/277

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "http://github.com/y42/clj-druid"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [swiss-arrows "1.0.0"]
-                 [clj-http "1.0.1"]
-                 [prismatic/schema "0.4.2"]
-                 [org.clojure/tools.logging "0.3.0"]
-                 [org.clojure/data.json "0.2.5"]
+                 [clj-http "2.0.0"]
+                 [prismatic/schema "0.4.4"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [org.clojure/data.json "0.2.6"]
                  [zookeeper-clj "0.9.1"]])
