6d2068ecf5eb2f19ea405c94f54a420179bdfccf
Guillaume Buisson <gbuisson@cisco.com>
2016-03-17 11:08:59 +0100

    added integration test selectors

--- project.clj
+++ project.clj
@@ -3,6 +3,12 @@
   :url "http://github.com/y42/clj-druid"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+
+  :test-selectors {:default (fn [m] (not (or (:integration m) (:regression m))))
+                   :integration :integration
+                   :regression :regression}
+
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [swiss-arrows "1.0.0"]
                  [clj-http "2.0.0"]
--- test/clj_druid/client_test.clj
+++ test/clj_druid/client_test.clj
@@ -3,21 +3,21 @@
             [clj-druid.validations-test :as f]
             [clj-druid.client :refer :all]))
 
-(deftest test-connect-zookeeper
+(deftest ^:integration test-connect-zookeeper
   (connect {:zk {:host "127.0.0.1:2181"
                  :discovery-path "/druid/discovery"
                  :node-type "broker"}}))
 
-(deftest test-connect-user
+(deftest ^:integration test-connect-user
   (connect {:hosts ["http://localhost:8082/druid/v2/"]}))
 
-(deftest test-zk-query
+(deftest ^:integration test-zk-query
   (connect {:zk {:host "127.0.0.1:2181"
                  :discovery-path "/druid/discovery"
                  :node-type "broker"}})
 
   (query randomized :groupBy f/valid-groupby-query))
 
-(deftest test-user-query
+(deftest ^:integration test-user-query
   (connect {:hosts ["http://127.0.0.1:8083/druid/v2/"]})
   (query randomized :groupBy f/valid-groupby-query :timeout 5000))
