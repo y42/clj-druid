b865034f1f875236e44c63edc03269ed3548da3d
Guillaume Buisson <guillaume.buisson@gmail.com>
2017-01-12 15:02:15 +0100

    version bump

--- examples/project.clj
+++ examples/project.clj
@@ -1,11 +1,9 @@
-(defproject y42/clj-druid-examples "0.2.3"
+(defproject y42/clj-druid-examples "0.2.12"
   :description "clj-druid-examples"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [clj-time "0.9.0"] ;; needed as `lein ring` is broken
                  [metosin/compojure-api "0.20.4"]
                  [metosin/ring-http-response "0.6.1"]
-                 [metosin/ring-swagger-ui "2.1.5-M2"]
-                 [y42/clj-druid "0.2.4"]]
+                 [y42/clj-druid "0.2.12"]]
   :ring {:handler handler/app}
   :uberjar-name "examples.jar"
   :uberwar-name "examples.war"
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject y42/clj-druid "0.2.17-SNAPSHOT"
+(defproject y42/clj-druid "0.2.17"
   :description "Clojure library for Druid.io"
   :url "http://github.com/y42/clj-druid"
   :license {:name "Eclipse Public License"
