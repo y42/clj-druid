c2fe8757c84e8a327fac79809d90c76edfd55322
Matthieu Sprunck <matthieu@vigiglobe.com>
2017-01-12 14:56:10 +0100

    Dependencies upgrade

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject y42/clj-druid "0.2.13-SNAPSHOT"
+(defproject y42/clj-druid "0.2.17-SNAPSHOT"
   :description "Clojure library for Druid.io"
   :url "http://github.com/y42/clj-druid"
   :license {:name "Eclipse Public License"
@@ -9,10 +9,10 @@
                    :integration :integration
                    :regression :regression}
 
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [swiss-arrows "1.0.0"]
-                 [clj-http "2.0.0"]
-                 [prismatic/schema "1.0.4"]
+                 [clj-http "3.4.1"]
+                 [prismatic/schema "1.1.3"]
                  [org.clojure/tools.logging "0.3.1"]
                  [org.clojure/data.json "0.2.6"]
                  [curator "0.0.6"]])
