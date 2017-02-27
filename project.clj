56066fa9f5d15382a919ef2245ef56ef5b59a802
Guillaume Buisson <guillaume.buisson@gmail.com>
2017-02-27 11:43:35 +0100

    0.2.18 Release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject y42/clj-druid "0.2.18-SNAPSHOT"
+(defproject y42/clj-druid "0.2.18"
   :description "Clojure library for Druid.io"
   :url "http://github.com/y42/clj-druid"
   :license {:name "Eclipse Public License"
